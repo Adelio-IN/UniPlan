d877332a8ee38a7b037088da3481eab185874868
LEE IN <2024011862@office.cju.ac.kr>
2025-04-30 18:29:58 +0900

    강의 요일 입력 기능 구현
    * 주석으로 변수명 변경 기록해둠.(변수명 생각해 볼 것)

--- src/src/cal.java
+++ src/src/cal.java
@@ -10,14 +10,19 @@ public class cal
         int count = sc.nextInt();
         sc.nextLine();
 
-        String[] strings = new String[count];
+        String[] strings = new String[count]; // 변수명 변경 요망
 
-        for (int i = 0; i < count; i++)
-        {
+        for (int i = 0; i < count; i++) {
             System.out.print((i + 1) + "번째 강의명을 입력하세요: ");
             strings[i] = sc.nextLine();
         }
+        String[] strings1 = new String[count]; // 변수명 변경 요망
 
+        for (int i = 0; i < count; i++)
+        {
+            System.out.print((i + 1) + "강의 요일을 입력하세요. Ex: 월, 화 ");
+            strings1[i] = sc.nextLine();
+        }
         System.out.println("강의 목록");
         for (String str : strings)
         {
