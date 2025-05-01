2d41cf15db8a8f99cdcf0535661cda614b1d9189
LEE IN <2024011862@office.cju.ac.kr>
2025-05-01 22:53:21 +0900

    - 기존 Main 클래스 삭제
    -> Cal 클래스를 Main 클래스로 Rename
    
    - Sort 클래스 생성
    
    * 정렬 기능 구현요망

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,15 +1,42 @@
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
-// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
-public class Main {
-    public static void main(String[] args) {
-        //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
-        // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP <shortcut actionId="Debug"/>을(를) 눌러 코드 디버그를 시작하세요. 1개의 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 중단점을 설정해 드렸습니다
-            // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
-            System.out.println("i = " + i);
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("강의 수를 입력하세요.");
+        int count = sc.nextInt();
+        sc.nextLine();
+
+        String[] Lecture_Name = new String[count];
+
+        for (int i = 0; i < count; i++) {
+            System.out.print((i + 1) + "번째 강의명을 입력하세요: ");
+            Lecture_Name[i] = sc.nextLine();
+        }
+
+        String[] Lecture_Day = new String[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            System.out.print((i + 1) + "강의 요일을 입력하세요. Ex: 월, 화 ");
+            Lecture_Day[i] = sc.nextLine();
+        }
+
+        int[] Lecture_Time = new int[count];
+        for (int i = 0; i < count; i++)
+        {
+            System.out.print((i + 1) + "강의 시간을 입력하세요. Ex: 10, 13");
+            Lecture_Time[i] = sc.nextInt();
+            sc.nextLine();
+        }
+
+        System.out.println("강의 요일");
+        for (int i = 0; i < count; i++)
+        {
+            System.out.println(Lecture_Name[i] + " " + Lecture_Day[i] + "요일 " + Lecture_Time[i] + "시");
         }
     }
 }
---
+++ src/src/Sort/Sort.java
@@ -0,0 +1,14 @@
+package Sort;
+class Avoid
+{
+    String name;
+    String day;
+    int time;
+
+    Avoid(String name, String day, int time)
+    {
+        this.name = name;
+        this.day = day;
+        this.time = time;
+    }
+}
