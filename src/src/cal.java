80451eb481f93dda9170bc0c21da59c78107853e
LEE IN <2024011862@office.cju.ac.kr>
2025-05-01 17:33:38 +0900

    - 변수명 변경
    - 강의 요일 출력 기능 구현

--- src/src/cal.java
+++ src/src/cal.java
@@ -10,21 +10,37 @@ public class cal
         int count = sc.nextInt();
         sc.nextLine();
 
-        String[] strings = new String[count]; // 변수명 변경 요망
+        String[] Lecture_Name = new String[count]; // 변수명 변경 요망
 
         for (int i = 0; i < count; i++) {
             System.out.print((i + 1) + "번째 강의명을 입력하세요: ");
-            strings[i] = sc.nextLine();
+            Lecture_Name[i] = sc.nextLine();
         }
-        String[] strings1 = new String[count]; // 변수명 변경 요망
+
+        String[] Lecture_Day = new String[count]; // 변수명 변경 요망
 
         for (int i = 0; i < count; i++)
         {
             System.out.print((i + 1) + "강의 요일을 입력하세요. Ex: 월, 화 ");
-            strings1[i] = sc.nextLine();
+            Lecture_Day[i] = sc.nextLine();
         }
+
+        int schedule = sc.nextInt();
+        for (int i = 0; i < count; i++)
+        {
+            System.out.print((i + 1) + "강의 시간을 입력하세요. Ex: 10, 13");
+            schedule = sc.nextInt();
+            sc.nextLine();
+        }
+
         System.out.println("강의 목록");
-        for (String str : strings)
+        for (String str : Lecture_Name)
+        {
+            System.out.println(str);
+        }
+
+        System.out.println("강의 요일");
+        for (String str : Lecture_Day)
         {
             System.out.println(str);
         }
