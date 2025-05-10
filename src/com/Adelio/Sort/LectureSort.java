136033f6183e913245f5914e8b8da6b28448dc23
Adelio <2024011862@office.cju.ac.kr>
2025-05-10 15:24:02 +0900

    정렬용

--- src/com/Adelio/Sort/LectureSort.java
+++ src/com/Adelio/Sort/LectureSort.java
@@ -5,5 +5,10 @@ import java.util.*;
 public class LectureSort
 {
     private static final List<String> Day = Arrays.asList("월", "화", "수", "목", "금", "토", "일");
+    
+    public static void sort_Day_Time(List<Sort> lecture)
+    {
+        lecture.sort(Comparator.comparingInt((Sort s) -> Day.indexOf(s.day)).thenComparingInt(s -> s.time));
+    }
 
 }
