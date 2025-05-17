714dd03e8d00a28692bb2c8075e33698023fe12c
Adelio <2024011862@office.cju.ac.kr>
2025-05-18 04:50:51 +0900

    교수님인지, 학생인지 입력받는 기능 추가

--- src/com/Adelio/UserInput.java
+++ src/com/Adelio/UserInput.java
@@ -54,6 +54,38 @@ public class UserInput
 {
     public static void main(String[] args)
     {
-        
+        Scanner sc = new Scanner(System.in);
+        List<SelectUser> userList = new ArrayList<>();
+
+        while(true)
+        {
+            System.out.println("사용자 모드를 선택하세요. [professor, student] * 종료하려면 exit를 입력하세요.");
+            String userType = sc.nextLine();
+
+            if(userType.equals("exit"))
+            {
+                break;
+            }
+            System.out.println("이름을 입력하세요.");
+            String name = sc.nextLine();
+
+            System.out.println("ID를 입력하세요.");
+            int id = Integer.parseInt(sc.nextLine());
+
+            if(userType.equals("Student"))
+            {
+                System.out.println("전공을 입력하세요.");
+                String major = sc.nextLine();
+                userList.add(new Student(name, id, major));
+            }
+            else if(userType.equals("Professor"))
+            {
+                System.out.println("교수님 입니다.");
+            }
+            else
+            {
+                System.out.println("잘못된 입력입니다.");
+            }
+        }
     }
 }
