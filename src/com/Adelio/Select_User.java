63d371577b1a76de9bc5fdbefe67b62472ab2663
Adelio <2024011862@office.cju.ac.kr>
2025-05-18 02:14:54 +0900

    학생 클래스

--- src/com/Adelio/Select_User.java
+++ src/com/Adelio/Select_User.java
@@ -1,20 +1,43 @@
 package com.Adelio;
 
-abstract class Select_User
-{
+abstract class SelectUser {
     private String name;
     private int id;
 
-    public String getName()
-    {
+    public SelectUser(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
         return name;
     }
 
-    public int getId()
-    {
+    public int getId() {
         return id;
     }
+
     public abstract void display();
 }
 
+class Student extends SelectUser {
+    private String major;
 
+    public Student(String name, int id, String major) {
+        super(name, id);
+        this.major = major;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    @Override
+    public void display() {
+        System.out.println("이름: " + getName() + ", ID: " + getId() + ", 전공: " + major);
+    }
+}
