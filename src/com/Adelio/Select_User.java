797d9abcfa88544b604eb74a898882072dffea8e
Adelio <2024011862@office.cju.ac.kr>
2025-05-18 01:51:44 +0900

    유저 선택 기능

---
+++ src/com/Adelio/Select_User.java
@@ -0,0 +1,20 @@
+package com.Adelio;
+
+abstract class Select_User
+{
+    private String name;
+    private int id;
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+    public abstract void display();
+}
+
+
