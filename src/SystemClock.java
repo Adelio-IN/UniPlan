7df84e153ab5f7cc5cb7ec85570f513e475b685e
Adelio <2024011862@office.cju.ac.kr>
2025-06-03 04:35:26 +0900

    메소드 정의

--- src/SystemClock.java
+++ src/SystemClock.java
@@ -9,8 +9,22 @@ public class SystemClock
     private static final ZoneId Zone_ID = ZoneId.of("Asia/Seoul");
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-    public static void setFixedTime(LocalDateTime fixedDateTime) {
+    public static void setFixedTime(LocalDateTime fixedDateTime)
+    {
         clock = Clock.fixed(fixedDateTime.atZone(Zone_ID).toInstant(), Zone_ID);
         System.out.println("🕒 시스템 시간이 수동으로 설정되었습니다: " + fixedDateTime.format(formatter) + " (KST)");
+    }
+    public static LocalDateTime now()
+    {
+        return LocalDateTime.now(clock);
+    }
+    public static  String getFormattedTime()
+    {
+        return now().format(formatter);
+    }
+    public static void resetSystemTime()
+    {
+        clock = Clock.system(Zone_ID); // KST 시스템 시간으로 복원
+        System.out.println("🕒 시스템 시간이 다시 기본값으로 설정되었습니다." + now().format(formatter) + " (KST)");
     }
 }
