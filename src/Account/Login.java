2d83f3d3d09b60d1cfb1defe868188484fc13eb9
Adelio-IN <2024011862@office.cju.ac.kr>
2025-06-05 13:08:10 +0900

    Login 기능 구현중

--- src/Account/Login.java
+++ src/Account/Login.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
+
 public class Login
 {
-    
+    private Scanner sc;
+
+    public Login()
+    {
+        this.sc = new Scanner(System.in);
+    }
+    public Login tryLogin()
+    {
+        System.out.println("---로그인을 시작합니다---");
+        System.out.print("아이디를 입력하세요.");
+        String id = this.sc.nextLine();
+
+        System.out.print("비밀번호를 입력하세요: ");
+        String password = sc.nextLine().trim();
+    }
 }
