f7be7b61d55ae0969b36a721d4a87ef714c90061
Adelio <2024011862@office.cju.ac.kr>
2025-06-08 18:09:46 +0900

    사용자 인증을 위한 SystemRoster 클래스 추가

---
+++ src/SystemRoster.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class SystemRoster
+{
+    private static final Map<String, String> allowUser = new HashMap<>();
+
+    public static void loadDataRoaster()
+    {
+        allowUser.put("202139079", "이인");;
+        allowUser.put("202508199", "박시현");;
+        allowUser.put("guest", "guest");;
+        allowUser.put("staff", "staff");;
+    }
+
+    public static boolean isMember(String number, String name)
+    {
+        return allowUser.containsKey(number) && allowUser.get(number).equals(name);
+    }
+}
